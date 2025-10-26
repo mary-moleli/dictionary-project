328ab4480b27443e553bb1d51b83dbc73b405494
mary-moleli <marymoleli15@gmail.com>
2025-10-26 13:22:10 +0200

    API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition...`);
+    let apiKey = "f437a67519o9abbba5b8c7t03f34cda0";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
