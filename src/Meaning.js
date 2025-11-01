572ad88b11a640744ceeaacc6dd8570c3df58053
mary-moleli <marymoleli15@gmail.com>
2025-11-01 20:47:48 +0200

    rendered meanings

---
+++ src/Meaning.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meanings);
+  return (
+    <div className="Meaning">
+      <h3>{props.meanings.partOfSpeech}</h3>
+      {props.meanings.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.defintion}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
