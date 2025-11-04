269e952cb3e2f0a9cf584e128eecee593990cb43
mary-moleli <marymoleli15@gmail.com>
2025-11-04 10:14:43 +0200

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ export default function Results(props) {
     return (
       <div className="results">
         <h2>{props.results.word}</h2>
-        {props.results.meanings.map(function (meaning, index) {
+        {props.results.meaning.map(function (meaning, index) {
           return (
             <div key={index}>
               <Meaning meaning={meaning} />
